222a71aea25d6c1f7d767fb659d7b6d43142019f
Sriharshak95 <harsha.daggron@gmail.com>
2023-04-22 14:27:15 +0530

    express app

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors(corsOptions));
 const authClient = new auth.OAuth2User({
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
-  callback: window.location.origin+"/callback",
+  callback: "https://incentive-app.onrender.com/callback",
   scopes: ["tweet.read", "users.read"],
 });
 
