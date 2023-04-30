e4411c68727d0f8e5a56a2588d68d99d61747487
Sriharshak95 <harsha.daggron@gmail.com>
2023-04-30 11:00:57 +0530

    api keys changed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors(corsOptions));
 const authClient = new auth.OAuth2User({
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
-  callback: "http://localhost:443/callback",
+  callback: "http://incentive-app.onrender.com/callback",
   scopes: ["tweet.read", "users.read"],
 });
 
